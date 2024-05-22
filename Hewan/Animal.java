373e385900c45fd73228657c4c8bc44ca48fc42d
Myuziroun <alifrajafatahillah15@gmail.com>
2024-05-23 06:00:59 +0700

    Menambahkan Family

---
+++ Hewan/Animal.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Hewan;
+
+/**
+ *
+ * @author Lenovo
+ */
+class Animal {
+    String makanan;
+    String suara;
+    
+    public Animal(String makanan, String suara){
+        System.out.println("Cetakan hewan yang belum jelas....");
+        this.makanan = makanan;
+        this.suara = suara;
+    }
+    void makan(){
+        System.out.println("...........");
+    }
+    void bersuara(){
+        System.out.println(".........");
+    }
+    
+}
